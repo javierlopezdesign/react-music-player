e88f145316a86f8d5a4935a58a9c2436a95b620b
javierlopezdesign <45286457+javierlopezdesign@users.noreply.github.com>
2021-11-17 09:33:16 +0000

    initialize project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/style.sass';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
